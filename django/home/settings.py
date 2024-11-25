902f00851d152e4e65c921d31b61bb917ff99c47
Josue Hernandez <josueother@gmail.com>
2024-11-24 23:52:43 -0800

    edited collected static location

--- django/home/settings.py
+++ django/home/settings.py
@@ -162,7 +162,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Compressor settings
 COMPRESS_ROOT = BASE_DIR / 'collected_static'
